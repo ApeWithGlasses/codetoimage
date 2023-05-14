0954734914b92a4ef65642ae0bf70b7e503a0ad0
ApeWithGlasses <wcgiraldo@gmail.com>
2023-05-13 22:30:34 -0500

    V1.0

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer>
+        <h4><a href="https://github.com/ApeWithGlasses/codetoimage.git"><img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="Logo github" /> CodeToImage</a></h4>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import CodeToImage from '../assets/CodeToImage.png';
+
+const Header = () => {
+  return (
+    <header>
+        <img src={CodeToImage} alt="CodeToImage logo" />
+    </header>
+  )
+}
+
+export default Header
